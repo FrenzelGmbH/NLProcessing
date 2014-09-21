a220003fd44eb517f6bb5a9b34e9eaaf87d38b72
Philipp Frenzel <philipp@frenzel.net>
2014-09-21 15:08:22 +0200

    introducing nlp classes
    adding the nlp classes

--- modules/spamdetector/controllers/SpamdetectorController.php
+++ modules/spamdetector/controllers/SpamdetectorController.php
@@ -2,11 +2,22 @@
 
 namespace app\modules\spamdetector\controllers;
 
+// Yii2
 use yii\rest\ActiveController;
+
+// NlpTools
+use NlpTools\Models\FeatureBasedNB;
+use NlpTools\Documents\TrainingSet;
+use NlpTools\FeatureFactories\DataAsFeatures;
+use NlpTools\Classifiers\MultinomialNBClassifier;
 
 class SpamdetectorController extends ActiveController
 {
 
+	/**
+	 * the document model class
+	 * @var string for the object class
+	 */
 	public $modelClass = 'app\modules\spamdetector\models\Document';
 
 }
